06c1073f90c64682dbb68852352260a08e8c8055
keisyaamalia <kkaysaamalia@gmail.com>
2026-05-14 14:57:38 +0700

    add while choice

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -74,6 +74,12 @@ int main()
     int value;
     while (choice != 5)
     {
-       
+       cout << "1.push\n";
+        cout << "2.pop\n";
+        cout << "3.peek\n";
+        cout << "4.isEmpty\n";
+        cout << "5.exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
     }
 };
