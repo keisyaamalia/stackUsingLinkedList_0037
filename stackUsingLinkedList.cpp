b7488480aed3e47ddf86cab66c012a162d8e97a5
keisyaamalia <kkaysaamalia@gmail.com>
2026-05-14 14:38:15 +0700

    add void pop

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -34,6 +34,9 @@ int push(int value)
     //pop operation: remove the topmost element from the stack
     void pop()
     {
-       
+       if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
     }
 };
